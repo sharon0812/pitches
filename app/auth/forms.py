7801da3408f2967a1cbf12d0585d3ad215b43e50
sharon0812 <anyangosharon@gmail.com>
2020-12-06 09:56:57 +0300

    modify auth folder

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -22,9 +22,9 @@ class RegistrationForm(FlaskForm):
 
     def validate_email(self,data_field):
         if User.query.filter_by(email =data_field.data).first():
-            raise ValidationError('There is an account with that email')
+            raise ValidationError('The email already exist')
 
     def validate_username(self,data_field):
         if User.query.filter_by(username = data_field.data).first():
-            raise ValidationError('That username is taken')
+            raise ValidationError('The username is taken')
         
